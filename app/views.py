506ec431a058ee7dea8360653c6b22dbaa3437ed
darleet <darleet@mail.ru>
2024-04-09 01:02:24 +0300

    Added views for questions

--- app/views.py
+++ app/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 
 
 # Create your views here.
+QUESTIONS = [
+    {
+        "id": i,
+        "title": f"Question {i}",
+        "text": f"This is question number {i}"
+    } for i in range(200)
+]
+
+
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'questions': QUESTIONS})
 
 
 def question(request, question_id):
-    return render(request, 'question.html')
+    return render(request, 'question.html', {'question': QUESTIONS[question_id]})
