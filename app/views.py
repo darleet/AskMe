5a4bebdc37f5b2b942f0d57d7038e63f895cf6ef
darleet <darleet@mail.ru>
2024-04-09 01:46:14 +0300

    Finished pagination

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 
@@ -10,10 +11,24 @@ QUESTIONS = [
     } for i in range(200)
 ]
 
+ANSWERS = [
+    {
+        "id": i,
+        "text": f"This is answer number {i}"
+    } for i in range(100)
+]
+
 
 def index(request):
-    return render(request, 'index.html', {'questions': QUESTIONS})
+    page_num = request.GET.get('page', 1)
+    paginator = Paginator(QUESTIONS, 5)
+    page = paginator.page(page_num)
+    return render(request, 'index.html', {'questions': page})
 
 
 def question(request, question_id):
-    return render(request, 'question.html', {'question': QUESTIONS[question_id]})
+    page_num = request.GET.get('page', 1)
+    paginator = Paginator(ANSWERS, 5)
+    page = paginator.page(page_num)
+    return render(request, 'question.html', {
+        'question': QUESTIONS[question_id], 'answers': page})
