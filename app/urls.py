bf42f57ad43e7ad4090b8bfd47b2baa64a8108a8
darleet <darleet@mail.ru>
2024-04-09 00:15:57 +0300

    Added question page

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('question/<int:question_id>/', views.question, name='question'),
 ]
--- app/views.py
+++ app/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def question(request, question_id):
+    return render(request, 'question.html')
