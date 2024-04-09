d98ec100bb70abdafc21bec34fe050e1bb2d7b3f
darleet <darleet@mail.ru>
2024-04-09 22:39:18 +0300

    Renamed django project

--- askme/asgi.py
+++ askme/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for AskMe project.
+ASGI config for askme project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AskMe.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'askme.settings')
 
 application = get_asgi_application()
--- askme/wsgi.py
+++ askme/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for AskMe project.
+WSGI config for askme project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AskMe.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'askme.settings')
 
 application = get_wsgi_application()
