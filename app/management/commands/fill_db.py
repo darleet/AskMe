735857161359d73081975a5acc9d6e82186ca880
darleet <darleet@mail.ru>
2024-04-10 08:57:04 +0300

    Added filling command fill_db

---
+++ app/management/commands/fill_db.py
@@ -0,0 +1,86 @@
+from django.core.management.base import BaseCommand
+from app.models import *
+
+from random import shuffle
+
+
+class Command(BaseCommand):
+    help = 'Fills the database with initial data'
+
+    def add_arguments(self, parser):
+        parser.add_argument('ratio', type=int, help='The ratio of data to fill')
+
+    def handle(self, *args, **options):
+        ratio = options['ratio']
+
+        Tag.objects.all().delete()
+        User.objects.all().delete()
+        Profile.objects.all().delete()
+        Question.objects.all().delete()
+        Answer.objects.all().delete()
+        QuestionVote.objects.all().delete()
+        AnswerVote.objects.all().delete()
+
+        self.stdout.write(f'Creating tags...')
+        tags = [Tag(name=f'tag_{i}') for i in range(ratio)]
+        Tag.objects.bulk_create(tags)
+
+        self.stdout.write(f'Creating profiles...')
+        users = [
+            User(
+                username=f'user_{i}',
+                password='django-test1234',
+                email=f'user_{i}@example.com',
+            ) for i in range(ratio)
+        ]
+        User.objects.bulk_create(users)
+        profiles = [
+            Profile(
+                user=users[i],
+                nickname=f'user_{i}',
+            ) for i in range(ratio)
+        ]
+        Profile.objects.bulk_create(profiles)
+
+        self.stdout.write(f'Creating questions...')
+        questions = [
+            Question(
+                title=f'Question {i}',
+                text=f'This is question number {i}',
+                author=profiles[i % ratio],
+            ) for i in range(ratio * 10)
+        ]
+        Question.objects.bulk_create(questions)
+        for i in range(len(questions)):
+            questions[i].tags.add(tags[i % ratio])
+
+        self.stdout.write(f'Creating answers...')
+        answers = [
+            Answer(
+                text=f'This is answer number {i}',
+                question=questions[i % (ratio * 10)],
+                author=profiles[i % ratio],
+            ) for i in range(ratio * 100)
+        ]
+        Answer.objects.bulk_create(answers)
+
+        self.stdout.write(f'Creating votes...')
+        qvotes = [
+            QuestionVote(
+                question=questions[i],
+                voter=profiles[i % ratio],
+                value=SCORES[i % 2][0],
+            ) for i in range(ratio * 10)
+        ]
+        QuestionVote.objects.bulk_create(qvotes)
+        avotes = [
+            AnswerVote(
+                answer=answers[i],
+                voter=profiles[i % ratio],
+                value=SCORES[i % 2][0],
+            ) for i in range(ratio * 100)
+        ]
+        AnswerVote.objects.bulk_create(avotes)
+
+        self.stdout.write(
+            self.style.SUCCESS(f'Database filled successfully'))
